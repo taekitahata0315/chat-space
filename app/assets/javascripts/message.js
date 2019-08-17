76e33f0c1516fe44d0e4675b7e4ccabdb9322a7b
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-08-17 15:23:48 +0900

    Automatic_updating
    # what
    自動更新実装
    
    #why
    実装の練習の為

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,9 @@
+$(document).on('turbolinks:load', function(){
+
 $(function () {
   function buildHTML(data) {
     var image = data.image.url ? `<img src="${data.image.url}">` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id= "${data.id}">
             <div class="message__upper-info">
             <div class="message__upper-info__talker">
             ${data.user_name}
@@ -10,15 +12,16 @@ $(function () {
               ${data.created_at}
               </div>
               </div>
-              <div class="message">
+              <div class="message-list">
               <p class="message__text">
                 ${data.content}</p>
                 ${image}
               </div>`
               
       return html;
-
 }
+
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -42,4 +45,31 @@ $(function () {
       alert('error');
     })
   })
+  var reloadMessages = function () {
+    if(location.pathname.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $(".message:last").data('message-id');
+      var group_id = $(".main-header__left-box__current-group").data("group-id");
+      console.log(last_message_id);
+    $.ajax({
+      url: `/groups/${group_id}/api/messages`,
+      type: 'get',
+      dataType: 'json',
+      data: {last_id: last_message_id}
+    })
+    .done(function (messages) {
+      var insertHTML ='';
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
+    });
+  })
+    .fail(function () {
+      // alert('自動更新に失敗しました');
+      console.log('error');
+    });
+  }
+};
+setInterval(reloadMessages, 5000);
+  });
 });
